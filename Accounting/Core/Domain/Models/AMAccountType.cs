4677c68d0179aff4ce6aecd58c4958636c7ce56f
Russel Madere <russel.madere@gmail.com>
2024-08-27 08:10:09 -0500

    Added Account models

--- Accounting/Core/Domain/Models/AMAccountType.cs
+++ Accounting/Core/Domain/Models/AMAccountType.cs
@@ -4,16 +4,34 @@
     {
         public byte Id { get; set; }
         public string Name { get; set; }
+        public String Code { get; set; }
         public NormalBalance NormalBalance { get; set; }
+        public String Category { get; set; }
+        public String IsActive { get; set; }
+        public String Description { get; set; }
+        public FinancialStatementType StatementType { get; set; }
+        public byte ReportingSequence { get; set; }
+        public String HasTaxImplications { get; set; }
 
-        public AMAccountType() : this(0, String.Empty, NormalBalance.D)
+        public List<AMAccount> Accounts { get; set; }
+
+        public AMAccountType() : this(0, String.Empty, String.Empty, NormalBalance.D, String.Empty, "N", String.Empty, FinancialStatementType.BalanceSheet, 0, "N")
         { }
 
-        public AMAccountType(byte id, String name, NormalBalance normalBalance)
+        public AMAccountType(byte id, String name, String code, NormalBalance normalBalance, String category, String isActive, String description, FinancialStatementType statementType, byte reportingSequence, String hasTaxImplications)
         {
             Id = id;
             Name = name;
+            Code = code;
             NormalBalance = normalBalance;
+            Category = category;
+            IsActive = isActive;
+            Description = description;
+            StatementType = statementType;
+            ReportingSequence = reportingSequence;
+            HasTaxImplications = hasTaxImplications;
+
+            Accounts = new List<AMAccount>();
         }
     }
 }
---
+++ Accounting/Core/Enums/FinancialStatementType.cs
@@ -0,0 +1,9 @@
+﻿namespace Accounting.Core
+{
+    public enum FinancialStatementType : byte
+    {
+        BalanceSheet = 1,
+        IncomeStatement = 2,
+        CashFlow = 3
+    }
+}
